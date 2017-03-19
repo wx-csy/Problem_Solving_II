89bff2eb3bf663f268778e22dcef6cd98d136400
Shaoyuan CHEN <sy_chen@smail.nju.edu.cn>
2017-03-19 11:04:41 +0800

    reupdate programs

--- programs/2-4/zipcode.cpp
+++ programs/2-4/zipcode.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <cstdio>
+#include <cctype>
 #include <numeric>
+#include <cassert>
 #include <cstring>
 using namespace std;
 
@@ -7,47 +10,63 @@ const char* codetable[10] = {
     "11000", "00011", "00101", "00110", "01001",
     "01010", "01100", "10001", "10010", "10100"
 };
-const int weight[5] = {7, 4, 2, 1, 0};
 
-int decode(const char* c){
-    int cnt = 0;
-    int ans;
-    int code[5];
+void decode(const char* c){
+    int ans[6];
+    char sections[5][8];
+    assert (c[5] == ' ' && c[11] == ' ' && c[17] == ' '
+        && c[23] == ' ');
+    sscanf(c, "%s %s %s %s %s",
+           sections[0], sections[1], sections[2], sections[3], sections[4]);
     for (int i=0; i<5; i++){
-        code[i]=c[i]-'0';
-        if (code[i]==1) cnt++;
+        for (int j=0; j<5; j++){
+            assert(sections[i][j] == '0' || sections[i][j] == '1');
+        }
     }
-    if (cnt != 2) return -1;
-    ans = inner_product(code, code+5, weight, 0);
-    if (ans == 11) ans = 0;
-    return ans;
+    for (int i=0; i<5; i++){
+        for (int j=0; j<10; j++){
+            if (strcmp(sections[i], codetable[j])==0){
+                ans[i] = j;
+                goto nf;
+            }
+        }
+        cout << "error" << endl;
+        return;
+nf:;
+    }
+    for (int i=0; i<5; i++){
+        cout << ans[i];
+    }
+    cout << endl;
+}
+
+void encode(char* str){
+    assert(strlen(str) == 5);
+    const char* sections[50];
+    for (int i=0; i<5; i++){
+        sections[i] = codetable[str[i]-'0'];
+    }
+    for (int i=0; i<5; i++){
+        cout << sections[i];
+        if (4-i) cout << ' ';
+    }
+    cout << endl;
 }
 
 int main(){
-char tmp[50];
-while (!cin.getline(tmp, 40).eof()){
-    int ans[10];
-    if (strlen(tmp) == 5){
-        cout << codetable[tmp[0]-'0'] << ' ';
-        cout << codetable[tmp[1]-'0'] << ' ';
-        cout << codetable[tmp[2]-'0'] << ' ';
-        cout << codetable[tmp[3]-'0'] << ' ';
-        cout << codetable[tmp[4]-'0'] << endl;
-    } else {
-        for (int i=0; i<5; i++){
-            ans[i] = decode(tmp + 6*i);
-            if (ans[i] < 0){
-                cout << "error" << endl;
-                goto nw;
+    char tmp[50];
+    int x;
+    while (!cin.getline(tmp, 40).eof()){
+        if (strlen(tmp)  == 5){
+            for (int i=0; i<5; i++){
+                assert(isdigit(tmp[i]));
             }
-
+            encode(tmp);
+        } else if (strlen(tmp) == 29) {
+            decode(tmp);
+        } else {
+            assert(false);
         }
-        for (int i=0; i<5; i++){
-            cout << ans[i];
-        }
-        cout << endl;
     }
-nw:;
-}
     return 0;
 }
